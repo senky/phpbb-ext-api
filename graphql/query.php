82331d1a953541072fbc69736d40198573f2e179
Jakub Senko <jakubsenko@gmail.com>
2019-03-25 16:49:39 +0100

    Add topics to query

--- graphql/query.php
+++ graphql/query.php
@@ -76,7 +76,7 @@ class query extends ObjectType
 						}
 
 						// we need forum_id if user requested forum data
-						if ($fetch_forum && empty($fields['forum_od']))
+						if ($fetch_forum && empty($fields['forum_id']))
 						{
 							$fields['forum_id'] = true;
 						}
@@ -102,6 +102,66 @@ class query extends ObjectType
 						}
 
 						return $row;
+					},
+				],
+				'topics'	=> [
+					'type'	=> types::listOf(types::topic()),
+					'args'	=> [
+						'topic_ids'	=> types::listOf(types::id()),
+					],
+					'resolve'	=> function($db, $args, $context, ResolveInfo $info) {
+						$fields = $info->getFieldSelection();
+
+						// forum is special field, we can't fetch it directly
+						if (isset($fields['forum']))
+						{
+							$fetch_forum = true;
+							unset($fields['forum']);
+						}
+
+						// we need forum_id if user requested forum data
+						if ($fetch_forum && empty($fields['forum_id']))
+						{
+							$fields['forum_id'] = true;
+						}
+
+						$sql = 'SELECT ' . implode(',', array_keys($fields)) . '
+							FROM ' . TOPICS_TABLE;
+
+						if (!empty($args['topic_ids']))
+						{
+							$sql .= ' WHERE ' . $db->sql_in_set('topic_id', $args['topic_ids']);
+						}
+
+						$result = $db->sql_query($sql);
+						$rows = $db->sql_fetchrowset($result);
+						$db->sql_freeresult($result);
+
+						if ($fetch_forum)
+						{
+							$fields = $info->getFieldSelection(1);
+							$fields = array_keys($fields['forum']);
+
+							$forum_ids = array_column($rows, 'forum_id');
+
+							$sql = 'SELECT forum_id, ' . implode(',', $fields) . '
+								FROM ' . FORUMS_TABLE . '
+								WHERE ' . $db->sql_in_set('forum_id', $forum_ids);
+							$result = $db->sql_query($sql);
+							$forum_rows = [];
+							while ($row = $db->sql_fetchrow($result))
+							{
+								$forum_rows[$row['forum_id']] = $row;
+							}
+							$db->sql_freeresult($result);
+
+							foreach ($rows as &$row)
+							{
+								$row['forum'] = $forum_rows[$row['forum_id']];
+							}
+						}
+
+						return $rows;
 					},
 				],
 			],
