8aee9e8c3dd11f469c5abd9f5538a8b432254789
Jakub Senko <jakubsenko@gmail.com>
2019-06-07 14:35:36 +0200

    Include debug information

--- controller/main_controller.php
+++ controller/main_controller.php
@@ -10,22 +10,33 @@
 
 namespace senky\api\controller;
 
-use senky\api\graphql\query;
-use senky\api\graphql\types;
 use GraphQL\Type\Schema;
-use GraphQL\GraphQL;
 use GraphQL\Error\Debug;
 use GraphQL\Server\StandardServer;
 
 class main_controller
 {
 	protected $request;
+	protected $db;
+	protected $config;
+	protected $user;
 	protected $context;
 	protected $query;
 	protected $mutation;
-	public function __construct(\phpbb\request\request $request, \senky\api\graphql\context $context, \senky\api\graphql\query $query, \senky\api\graphql\mutation $mutation)
+	public function __construct(
+		\phpbb\request\request $request,
+		\phpbb\db\driver\driver_interface $db,
+		\phpbb\config\config $config,
+		\phpbb\user $user,
+		\senky\api\graphql\context $context,
+		\senky\api\graphql\query $query,
+		\senky\api\graphql\mutation $mutation
+	)
 	{
 		$this->request = $request;
+		$this->db = $db;
+		$this->config = $config;
+		$this->user = $user;
 		$this->context = $context;
 		$this->query = $query;
 		$this->mutation = $mutation;
@@ -42,6 +53,42 @@ class main_controller
 			'context'	=> $this->context,
 			'debug'		=> Debug::INCLUDE_DEBUG_MESSAGE | Debug::INCLUDE_TRACE,
 		]);
-		$server->handleRequest(null, true);
+		$result = $server->executeRequest();
+
+		if (defined('PHPBB_DISPLAY_LOAD_TIME'))
+		{
+			if (isset($GLOBALS['starttime']))
+			{
+				$totaltime = microtime(true) - $GLOBALS['starttime'];
+				$result->data['debug']['time'] = [
+					'sql'	=> $this->db->get_sql_time(),
+					'php'	=> $totaltime - $this->db->get_sql_time(),
+					'total'	=> $totaltime,
+				];
+			}
+
+			$result->data['debug']['queries'] = [
+				'cached'	=> $this->db->sql_num_queries(true),
+				'total'		=> $this->db->sql_num_queries(),
+			];
+
+			$memory_usage = memory_get_peak_usage();
+			if ($memory_usage)
+			{
+				$result->data['debug']['peak_memory_usage'] = $memory_usage;
+			}
+		}
+
+		if (defined('DEBUG'))
+		{
+			$result->data['debug']['gzip'] = $this->config['gzip_compress'] && @extension_loaded('zlib');
+
+			if ($this->user->load)
+			{
+				$result->data['debug']['load'] = $this->user->load;
+			}
+		}
+
+		$server->getHelper()->sendResponse($result, true);
 	}
 }
